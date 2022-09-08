18cc60eb72a224cb6d620cea1c12e7744bee36f3
seugirdorx <amandagetion@gmail.com>
2022-09-08 13:43:49 -0300

    Programa de saúde

---
+++ index 1.js
@@ -0,0 +1,6 @@
+let HelloWord
+//alert(nome)
+//console.log(nome)
+HelloWord = prompt("Escreva o seu nome");
+alert ("Hello Word")
+alert ("Seu nome é: " + nome + "\n" + "Sua idade é: " + idade + "\n" + "Seu gosto musical é: " + gostoMusical + "\n" + "Seu filme favorito é: " + filmequantidade de filhos: " + filhos + "\n" + "Esse é o seu sonho: " + sonho);
